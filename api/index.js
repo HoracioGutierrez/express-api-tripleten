8d166de28355fa889e51222f89863d21a0bba330
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-08-22 12:32:05 -0300

    Add new users/me route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/users/me", (req, res) => {
+  res.json({
+    "name": "John Doe",
+    "email": "john@example.com",
+    "phone": "555-555-5555"
+  })
+});
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
